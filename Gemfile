42d74dce2153728672fd2e8a84fa584719fb19d9
Emmanuel Crisóstomo <crisostomoemmanuel@gmail.com>
2015-06-23 16:59:47 -0400

    Added Bootstrap for styling

--- Gemfile
+++ Gemfile
@@ -27,6 +27,15 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+# Use boostrap sass
+gem "bootstrap-sass", "~> 3.3"
+
+# Use font-awesome-rails
+gem "font-awesome-rails", "~> 4.3"
+
+# Use Simple form
+gem "simple_form", "~> 3.1.0"
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- spec/feature/creating_projects_spec.rb
+++ spec/feature/creating_projects_spec.rb
@@ -7,8 +7,7 @@ RSpec.feature "Users can create new projects" do
 		click_link "New Project"
 	end
 
-	scenario "with valid attributes" do
-		
+	scenario "with valid attributes" do		
 		fill_in "Name", with: "Sublime Text 3"
 		fill_in "Description", with: "A text editor for everyone"
 		click_button "Create Project"
@@ -23,9 +22,9 @@ RSpec.feature "Users can create new projects" do
 	end
 
 	scenario "when providing invalid attributes" do
-	click_button "Create Project"
-	
-	expect(page).to have_content "Project has not been created."
-	expect(page).to have_content "Name can't be blank"
+		click_button "Create Project"
+		
+		expect(page).to have_content "Project has not been created."
+		expect(page).to have_content "Name can't be blank"
 	end
 end
