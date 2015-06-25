650a365a703aef5c8dba3174d2ae7e53db8faa8b
Emmanuel Crisóstomo <crisostomoemmanuel@gmail.com>
2015-06-25 18:09:54 -0400

    Link tickets and users upon ticket creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,36 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624154106) do
+ActiveRecord::Schema.define(version: 20150625194359) do
 
-  create_table "projects", force: true do |t|
-    t.string   "name"
-    t.string   "description"
+  create_table "projects", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "tickets", force: true do |t|
-    t.string   "name"
+  create_table "tickets", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.text     "description"
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "author_id"
   end
 
+  add_index "tickets", ["author_id"], name: "index_tickets_on_author_id"
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
